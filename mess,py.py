5ac1c7b4b94c9d4a5536d05252b65d3e75f6c7e0
rahulambastha23 <47660381+rahulambastha23@users.noreply.github.com>
2019-02-27 11:13:54 +0530

    Add files via upload

--- mess,py.py
+++ mess,py.py
@@ -1,9 +1,9 @@
 import requests
-ar=[2,3,4,5,6,7]
+
 url = "https://www.fast2sms.com/dev/bulk"
-payload = "sender_id=FSTSMS&message=ar&language=english&route=p&numbers=8770631461"
+payload = "sender_id=FSTSMS&message=hellobrother&language=english&route=p&numbers=9340284683,9171039045,9340793983,8770631461"
 headers = {
-'authorization': "APBJf5rZFGS9UCw41xLmWzElyRucQ8q7dsYIvKtp2nVb6HXeDgdcHnIRqLfDjAFmazyJxrSKZ0s5Xip2",
+'authorization': "9ghEGQ3pI8FquHZo5lMi2XtJK07x6AndPVevjYNfDBkmsCzULb5q3sBDuHIo29hQRUbNJXr1S4FwmzAg",
 'Content-Type': "application/x-www-form-urlencoded",
 'Cache-Control': "no-cache",
 }
